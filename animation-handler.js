665151a974806235f150e757d8c8901e87bca787
IanCarrasco <iansee.c@gmail.com>
2017-02-26 12:04:48 -0800

    Added close toggle

--- animation-handler.js
+++ animation-handler.js
@@ -3,4 +3,11 @@ $(".nav-button").click(function(e){
 	$("#name-container").toggleClass("full");
 	$("#name").html((e.target.innerText));
 	$(".content-box").css({"opacity" : "1"}).delay(1000).show(1);
+	$("#close-expand").css({"visibility":"visible","opacity":"1"})
+});
+$("#close-expand").click(function(){
+	$("#name-container").toggleClass("full");
+	$("#name").delay(500).html("ian carrasco");
+	$("#close-expand").css({"visibility":"hidden","opacity":"0"})
+
 });
