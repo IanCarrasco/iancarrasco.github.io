1b3cfd61c0a6231676f4b385b82785f86f2fe654
IanCarrasco <iansee.c@gmail.com>
2017-02-26 18:55:54 -0800

    Added fadeout nav

--- animation-handler.js
+++ animation-handler.js
@@ -4,12 +4,13 @@ $(".nav-button").click(function(e){
 	$("#name").html((e.target.innerText));
 	$(".content-box").css({"opacity" : "1"}).delay(1000).show(1);
 	$("#close-expand").css({"visibility":"visible","opacity":"1"})
+	$("#nav-holder").fadeOut(500)
 });
 $("#close-expand").click(function(){
 	$("#name-container").toggleClass("full");
 	$("#name").delay(500).html("ian carrasco");
 	$("#close-expand").css({"visibility":"hidden","opacity":"0"})
-
+	$("#nav-holder").fadeIn(500)
 });
 
 //PARTICLE BACKGROUND
