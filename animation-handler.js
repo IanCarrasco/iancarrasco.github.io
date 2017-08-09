1601023275e73594609c94429ee3d4b3db52c6dd
Ian Carrasco <iansee.c@gmail.com>
2017-08-09 01:24:11 -0700

    Added Hash Routing

--- animation-handler.js
+++ animation-handler.js
@@ -1,22 +1,49 @@
 
 $("#content-frame").hide()
-$(".nav-button").click(function(e){
-	$("#name-container").toggleClass("full");
-	$("#name").html((e.target.innerText));
-	document.getElementById('content-frame').src = e.target.innerText +'.html'
-	document.title = "Ian Carrasco | " + e.target.innerText;
+window.onload = () =>{
+	if(history.pushState) {
+    	history.pushState(null, null, '#home');
+	}
+	else {
+		location.hash = '#home';
+	}
+}
+
+var ExpandNav = (name) =>{
+	$("#nav-holder").fadeOut(500).delay(1000);
+	$("#name-container").addClass("full");
+	$("#name").html((name));
+	document.getElementById('content-frame').src = name +'.html'
+	document.title = "Ian Carrasco | " + name;
 	$(".content-box").css({"opacity" : "1"}).delay(1000).show(1);
 	$("#close-expand").css({"visibility":"visible","opacity":"1"})
-	$("#nav-holder").fadeOut(500).delay(1000);
+	
 	$("#content-frame").css({zIndex:99})
-	$("#content-frame").delay(500).fadeIn(200)
-});
-$("#close-expand").click(function(){
-	$("#name-container").toggleClass("full");
+	$("#content-frame").delay(500).fadeIn(100)
+}
+var CollapseNav = () =>{
+	$("#name-container").removeClass("full");
 	$("#name").delay(500).html("ian<br>carrasco");
 	$("#close-expand").css({"visibility":"hidden","opacity":"0"})
 	$("#nav-holder").fadeIn(500)
 	$("#content-frame").fadeOut(200)
+}
+
+
+routie('projects', function() {
+	ExpandNav('projects')
+});
+routie('work', function() {
+	ExpandNav('work')
+});
+routie('resume', function() {
+	ExpandNav('resume')
+});
+routie('contact', function() {
+	ExpandNav('contact')
+});
+routie('home', function() {
+	CollapseNav()
 });
 
 //PARTICLE BACKGROUND
