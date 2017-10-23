bcac6dca555377433710261b1762ff9541be6dc8
Ian Carrasco <iansee.c@gmail.com>
2017-10-23 10:06:48 -0700

    Added new functionality

--- animation-handler.js
+++ animation-handler.js
@@ -1,10 +1,41 @@
 
 $("#content-frame").hide()
+function pagination(){
+	var pages = ["home","work","resume","projects","contact"]
+	var page_no = pages.indexOf(window.location.hash.substring(1));
+	console.log(page_no)
+	if(page_no==0){
+		$("#prev_name").text('')
+		$("#next_name").text('')
+	}
+	else if(page_no>0 && page_no<4){
+		$("#prev_name").text(pages[page_no-1])
+		$("#next_name").text(pages[page_no+1])
+		$("#prev_name").click(()=>{
+			window.location.href = "#" + pages[page_no-1];			
+		})
+		$("#next_name").click(()=>{
+			window.location.href = "#" + pages[page_no+1];			
+		})
+	}
+	else if(page_no==4){
+		$("#prev_name").text(pages[page_no-1])
+		$("#prev_name").click(()=>{
+			window.location.href = "#" + pages[page_no-1];			
+		})
+	}
+}
 window.onload = () =>{
 	if(window.location.href == 'http://iancarras.co/'){
 		routie('home')
 	}
+	pagination()
 }
+window.onhashchange = function() {
+	pagination()
+}
+
+
 
 var ExpandNav = (name) =>{
 	$("#nav-holder").fadeOut(500).delay(1000);
@@ -19,6 +50,16 @@ var ExpandNav = (name) =>{
 	$("#content-frame").delay(500).fadeIn(100)
 }
 var CollapseNav = (name) =>{
+	var pages = ["work","resume","projects","contact"]
+	var page_no = pages.indexOf(name);
+	if(page_no>0){
+		$("#prev_name").text('')
+		$("#next_name").text('')
+	}
+	else{
+		$("#next_name").text('')
+		$("#prev_name").text('')
+	}
 	$("#name-container").removeClass("full");
 	document.title = "Ian Carrasco | " + name;
 	$("#name").delay(500).html("ian<br>carrasco");
