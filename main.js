9e1743253bb0d945d1bf728d2b3be0f820517cbf
Ian Carrasco <iansee.c@gmail.com>
2018-01-01 01:22:54 -0800

    Added Animations

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+
+  $('.section-header').addClass('animated');
+  $('#site-title').addClass('animated');
+  $('.fa').addClass('fadeInUp animated');
+
+})
+
+console.log("hello")
