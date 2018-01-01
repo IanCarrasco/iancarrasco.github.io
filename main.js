951079cb72e15e7619be895111e84e513f458686
Ian Carrasco <iansee.c@gmail.com>
2018-01-01 02:09:28 -0800

    Fixed Some Scrolling Issues

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ $(document).ready(()=>{
   $('.section-header').addClass('animated');
   $('#site-title').addClass('animated');
   $('.fa').addClass('fadeInUp animated');
+  setTimeout(()=>{
+    
+    $('.section-header').removeClass('animated');
+    $('#site-title').removeClass('animated');
+    $('.fa').removeClass('fadeInUp animated');
+
+  }, 3000)
 
 })
-$('.section-header').removeClass('animated');
-$('#site-title').removeClass('animated');
-$('.fa').removeClass('fadeInUp animated');
+
+
 console.log("hello")
