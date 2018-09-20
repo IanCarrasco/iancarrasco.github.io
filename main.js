81c8992493658a36f1cb5134581935b6bb588593
Ian Carrasco <iansee.c@gmail.com>
2018-09-20 16:36:51 -0700

    Added scrolling behavior

--- main.js
+++ main.js
@@ -1,17 +1,59 @@
-$(document).ready(()=>{
-
-  $('.section-header').addClass('animated');
-  $('#site-title').addClass('animated');
-  $('.fa').addClass('fadeInUp animated');
-  setTimeout(()=>{
-    
-    $('.section-header').removeClass('animated');
-    $('#site-title').removeClass('animated');
-    $('.fa').removeClass('fadeInUp animated');
-
-  }, 3500)
-
-})
+const pages = ["#about", "#projects", "#work", "#contact"]
 
 
-console.log("hello")
+
+
+window.onload = function() { 
+  
+  let selector = `div[href="${window.location.hash}"]`
+
+  let prev = window.location.hash
+  window.location.hash = ""
+  window.location.hash = prev
+
+  $(selector)[0].scrollIntoView({
+    behavior: 'smooth',
+    block:'start'
+  });
+
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
+  });
+
+}
+
+window.onhashchange = function() { 
+
+  let selector = `div[href="${window.location.hash}"]`
+
+  $(selector)[0].scrollIntoView({
+    behavior: 'smooth',
+    block:'start'
+  });
+}
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38' && pages.indexOf(window.location.hash) != 0) {
+        // up arrow
+        window.location.hash = pages[(pages.indexOf(window.location.hash) - 1) % pages.length]
+
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        window.location.hash = pages[(pages.indexOf(window.location.hash) + 1) % pages.length]
+
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+    }
+
+}
